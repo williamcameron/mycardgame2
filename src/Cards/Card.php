a888c149eb2783f2b6558a12a1a25dd38d216edc
William Cameron <wp.cameron@gmail.com>
2017-03-27 00:17:01 +0100

    add name construct param to card class

--- src/Cards/Card.php
+++ src/Cards/Card.php
@@ -4,6 +4,11 @@ namespace Game\Cards;
 
 class Card
 {
+
+    public function __construct($name="Unknown"){
+       $this->name = $name;
+    }
+
     public function pokemon(){
         return false;
     }
